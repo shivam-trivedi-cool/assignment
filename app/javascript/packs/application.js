30c55e63901acf4daa5073f8ee860da988f96c49
Saddam Husain <saddam20.hussain@gmail.com>
2021-01-26 18:34:29 +0530

    layout refactored

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,4 +1,12 @@
+import $ from 'jquery';
+window.jQuery = $;
+window.$ = $;
 import 'bootstrap'
+require("packs/animsition.min")
+require("packs/select2")
+require("packs/moment.min")
+require("packs/daterangepicker")
+require("packs/countdowntime")
 require("packs/main")
 require("@rails/ujs").start()
 require("turbolinks").start()
